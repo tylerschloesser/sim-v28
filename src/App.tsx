b8c546563374affc15bbd6c98fcc61e3c6fdbe63
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-07 11:04:18 -0800

    refactor app state

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useImmer } from "use-immer";
-import { generateWorld } from "./worldGen";
 import { TileGrid } from "./TileGrid";
 import { TileHighlight } from "./TileHighlight";
 import { useKeyboard } from "./useKeyboard";
@@ -7,40 +6,8 @@ import { useCrafting } from "./useCrafting";
 import { DebugOverlay } from "./DebugOverlay";
 import { ProgressBar } from "./ProgressBar";
 import { InventoryModal } from "./InventoryModal";
-import { updateViewport, updateVisibleChunks } from "./viewportUtils";
+import { initializeAppState } from "./initializeAppState";
 import type { AppState, Recipe } from "./types";
-import { WORLD_SIZE, TILE_SIZE } from "./constants";
-
-function initializeAppState(): AppState {
-  const world = generateWorld(WORLD_SIZE);
-  const worldCenterX = (WORLD_SIZE * TILE_SIZE) / 2;
-  const worldCenterY = (WORLD_SIZE * TILE_SIZE) / 2;
-
-  const state: AppState = {
-    player: { x: worldCenterX, y: worldCenterY, vx: 0, vy: 0 },
-    world,
-    viewport: { x: 0, y: 0, width: 0, height: 0 },
-    visibleChunks: { minChunkX: 0, maxChunkX: 0, minChunkY: 0, maxChunkY: 0 },
-    action: null,
-    inventory: {
-      stone: 0,
-      wood: 0,
-      iron: 0,
-      copper: 0,
-      coal: 0,
-      "stone-furnace": 0,
-      "wood-storage": 0,
-    },
-    inventoryOpen: false,
-    craftQueue: [],
-  };
-
-  // Calculate initial viewport and chunks
-  updateViewport(state);
-  updateVisibleChunks(state);
-
-  return state;
-}
 
 export function App() {
   const [state, setState] = useImmer<AppState>(initializeAppState);
---
+++ src/initializeAppState.ts
@@ -0,0 +1,35 @@
+import { generateWorld } from "./worldGen";
+import { updateViewport, updateVisibleChunks } from "./viewportUtils";
+import type { AppState } from "./types";
+import { WORLD_SIZE, TILE_SIZE } from "./constants";
+
+export function initializeAppState(): AppState {
+  const world = generateWorld(WORLD_SIZE);
+  const worldCenterX = (WORLD_SIZE * TILE_SIZE) / 2;
+  const worldCenterY = (WORLD_SIZE * TILE_SIZE) / 2;
+
+  const state: AppState = {
+    player: { x: worldCenterX, y: worldCenterY, vx: 0, vy: 0 },
+    world,
+    viewport: { x: 0, y: 0, width: 0, height: 0 },
+    visibleChunks: { minChunkX: 0, maxChunkX: 0, minChunkY: 0, maxChunkY: 0 },
+    action: null,
+    inventory: {
+      stone: 0,
+      wood: 0,
+      iron: 0,
+      copper: 0,
+      coal: 0,
+      "stone-furnace": 0,
+      "wood-storage": 0,
+    },
+    inventoryOpen: false,
+    craftQueue: [],
+  };
+
+  // Calculate initial viewport and chunks
+  updateViewport(state);
+  updateVisibleChunks(state);
+
+  return state;
+}
