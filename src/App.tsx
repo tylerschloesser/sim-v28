2ae0cf5b639a5404fd10e066d216ac927fbf9ac5
Tyler Schloesser <tylerschloesser@gmail.com>
2025-11-07 07:00:08 -0800

    use svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
 export function App() {
-  return null;
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        background: "#000000",
+      }}
+    >
+      <circle cx="50%" cy="50%" r="50" fill="#0000ff" />
+    </svg>
+  );
 }
